8173db3202077edd6069f0d4917b5b6b9318ec06
Alex <alx@dd19.de>
2009-09-01 01:27:44 +0200

    build script less ugly and less buggy

--- buildstuff/init.sh
+++ buildstuff/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #archs="brcm-2.4"
 #archs="atheros"
-archs="atheros brcm-2.4 x86"
+archs="brcm-2.4 atheros x86"
 echo $archs>ARCHS.txt
 # dirs
 mkdir dl
@@ -9,32 +9,33 @@ mkdir dl
 
 timestamp=`date "+%F_%H-%M"`
 echo $timestamp >timestamp
-date +"%Y/%m/%d %H:%M">VERSION.txt
+date +"%Y/%m/%d %H:%M">VERSION.txt||exit
 
 #get basic repos
 #git clone git://nbd.name/packages.git
-git clone git://github.com/alxhh/piratenluci.git
+git clone git://github.com/alxhh/piratenluci.git||exit
 
 mkdir $timestamp
 for arch in $archs;
 do
-	git clone git://nbd.name/8.09.git
-	ln -s ../../dl 8.09/dl
+	git clone git://nbd.name/8.09.git||exit
+	ln -s ../../dl 8.09/dl||exit
 #	echo src-link packages $PWD/packages>8.09/feeds.conf
-	echo src-link luci $PWD/piratenluci>8.09/feeds.conf
+	echo src-link luci $PWD/piratenluci>8.09/feeds.conf||exit
 	cd 8.09
-	ln -s ../piratenluci/patches .
-	quilt push -a
-	cp -Rv ../piratenluci/files .
-	./scripts/feeds update
-	./scripts/feeds install -a -p luci
+	./scripts/feeds update||exit
+	./scripts/feeds install -a -p luci||exit
+	cp ../piratenluci/buildstuff/$arch/build.config .config||exit
+	cp -R ../piratenluci/patches .||exit
+	cp ../piratenluci/buildstuff/$arch/series patches/series||exit
+	quilt push -a||exit
 	cd ..
-	cp piratenluci/buildstuff/$arch/build.config 8.09/.config
+	cp -Rv piratenluci/files 8.09||exit
 	config_arch=`( . 8.09/.config; echo $CONFIG_ARCH;)`
 	echo src/gz freifunk http://houston.dd19.de:81/~alx/piraten/$timestamp/$arch/packages/$config_arch>>8.09/files/etc/opkg.conf
 # update host
-	mkdir 8.09/files/etc/uci-defaults
+	mkdir 8.09/files/etc/uci-defaults||exit
 	echo uci set freifunk.upgrade.repository=http://houston.dd19.de/~alx/piraten/latest>8.09/files/etc/uci-defaults/piratenupdate
 	echo uci commit freifunk >>8.09/files/etc/uci-defaults/piratenupdate
-	mv 8.09 $timestamp/$arch
+	mv 8.09 $timestamp/$arch||exit
 done
