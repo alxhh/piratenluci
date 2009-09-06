475f28b617c1703024de36b2ab8291510d9d41f9
Alex <alx@dd19.de>
2009-09-06 16:34:23 +0200

    now upgrade should really work ;)

--- buildstuff/init.sh
+++ buildstuff/init.sh
@@ -29,10 +29,10 @@ do
 	cp -R ../piratenluci/patches .||exit
 	cp ../piratenluci/buildstuff/$arch/series patches/series||exit
 	quilt push -a||exit
-	cd ..
-	cp -Rv piratenluci/files 8.09||exit
+	cp -Rv ../piratenluci/files .||exit
 	echo Based on svn revision: `./scripts/getver.sh`>> files/etc/banner||exit
 	echo Built $timestamp on `hostname`>> files/etc/banner||exit
+	cd ..
 	config_arch=`( . 8.09/.config; echo $CONFIG_ARCH;)`
 	echo src/gz freifunk http://firmware.piratenfreifunk.de/piratenfreifunk/$timestamp/$arch/packages/$config_arch>>8.09/files/etc/opkg.conf
 # update host
